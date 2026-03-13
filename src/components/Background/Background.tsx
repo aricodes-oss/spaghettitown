9255c331492dc5db806d119147ae5be5a68753dd
Aria Taylor <ari@aricodes.net>
2026-03-12 17:43:39 -0700

    Make the number of background sparkles dependent on screen size

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useMemo } from 'react';
+import { useMatches } from '@mantine/core';
 
 import styles from './Background.module.css';
 import { Sparkle, generateSparkles } from './Sparkle';
@@ -9,8 +10,10 @@ interface BackgroundProps {
   sparkles?: number;
 }
 
-export function Background({ sparkles = 24 }: BackgroundProps) {
-  const sparkleList = useMemo(() => generateSparkles(sparkles), [sparkles]);
+export function Background({ sparkles }: BackgroundProps) {
+  const defaultSparkles = useMatches({ base: 12, sm: 24, md: 36, lg: 48, xl: 60 });
+  const count = sparkles ?? defaultSparkles;
+  const sparkleList = useMemo(() => generateSparkles(count), [count]);
 
   return (
     <div className={styles.background}>
