54134f2ed7d945fa684692a4b4ecb269754dcf1f
Aria Taylor <ari@aricodes.net>
2026-03-12 17:29:46 -0700

    Add page design, refactor sparkles

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,21 +1,22 @@
 'use client';
 
 import { useMemo } from 'react';
-import styles from "./Background.module.css";
-import { generateSparkles, Sparkle } from "./Sparkle";
+
+import styles from './Background.module.css';
+import { Sparkle, generateSparkles } from './Sparkle';
 
 interface BackgroundProps {
-	sparkles?: number;
+  sparkles?: number;
 }
 
-export function Background({ sparkles = 12 }: BackgroundProps) {
-	const sparkleList = useMemo(() => generateSparkles(sparkles), [sparkles]);
+export function Background({ sparkles = 24 }: BackgroundProps) {
+  const sparkleList = useMemo(() => generateSparkles(sparkles), [sparkles]);
 
-	return (
-		<div className={styles.background}>
-			{sparkleList.map((s, i) => (
-				<Sparkle key={i} x={s.x} y={s.y} delay={s.delay} scale={s.scale} />
-			))}
-		</div>
-	);
+  return (
+    <div className={styles.background}>
+      {sparkleList.map((s, i) => (
+        <Sparkle key={i} x={s.x} y={s.y} delay={s.delay} scale={s.scale} />
+      ))}
+    </div>
+  );
 }
