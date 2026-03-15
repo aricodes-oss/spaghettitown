a96b6dadf3f99a2caf6d300bf211dc7e925d0b90
Aria Taylor <ari@aricodes.net>
2026-03-15 15:52:31 -0700

    Add main content segment to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,51 @@
+'use client';
+
+import { Anchor, Box, Flex, List, Text, Title } from '@mantine/core';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import demoArt from '../../public/demo-art.jpg';
+import styles from './Home.module.scss';
+
 export default function Home() {
-  return null;
+  return (
+    <Box className={styles.root} px={8} py={4}>
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        justify={{ base: 'flex', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}
+        gap={{ base: 2, md: 4 }}
+      >
+        <Image src={demoArt} alt="What A Shame (Demo) art" className={styles.art} />
+        <Box px={4}>
+          <Title order={4} ta="center">
+            WELCOME TO OUR WEB PAGE!!!!!
+          </Title>
+          <Text>We are Choosing Clarity!!! Only one of us can play guitar.</Text>
+          <Text fw={700}>UPDATES:</Text>
+          <List icon="- ">
+            <List.Item>
+              <Text fw={700} span>
+                03/??:
+              </Text>{' '}
+              Talk Talk (Demo) coming out
+            </List.Item>
+            <List.Item>
+              <Text fw={700} span>
+                01/26:
+              </Text>{' '}
+              <Anchor
+                component={Link}
+                c="purple"
+                href="https://distrokid.com/hyperfollow/chasingclarity2/what-a-shame-demo"
+              >
+                What A Shame (Demo)
+              </Anchor>{' '}
+              released!!!
+            </List.Item>
+          </List>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
